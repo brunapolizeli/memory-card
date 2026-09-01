7150453974ef01a4c657aa44d7317f827d5fe121
Bruna Polizeli <66567923+cloudcastaway@users.noreply.github.com>
2026-08-31 22:50:48 -0300

    feat: prepare backend for Vercel deployment

---
+++ client/js/config.js
@@ -0,0 +1,1 @@
+export const API_BASE_URL = "https://memory-card-server.vercel.app/";
--- server/server.js
+++ server/server.js
@@ -4,17 +4,24 @@ import { pool } from "./db/pool.js";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.js";
 import gamesRouter from "./routes/games.js";
+import libraryRouter from "./routes/library.js";
 
 const app = express();
 const PORT = 3000;
+const allowedOrigins = process.env.FRONTEND_URL.split(",");
 
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/auth", authRouter);
 app.use("/games", gamesRouter);
+app.use("/library", libraryRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (!process.env.VERCEL) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
