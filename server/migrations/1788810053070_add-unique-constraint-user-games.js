af721427604275ef434e3b549a9e46ab15ed4f97
Bruna Polizeli <66567923+cloudcastaway@users.noreply.github.com>
2026-09-07 16:41:49 -0300

    fix: prevent duplicate games in user library with unique constraint

---
+++ server/migrations/1788810053070_add-unique-constraint-user-games.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.addConstraint("user_games", "unique_user_game", {
+    unique: ["user_id", "game_id"],
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropConstraint("user_games", "unique_user_game");
+};
