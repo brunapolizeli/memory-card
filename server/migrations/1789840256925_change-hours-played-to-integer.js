7dfd51988646ca0e6711daef44193dd9d52703f7
Bruna Polizeli <66567923+cloudcastaway@users.noreply.github.com>
2026-09-19 14:53:56 -0300

    feat: add minutes_played column and convert hours_played to integer

---
+++ server/migrations/1789840256925_change-hours-played-to-integer.js
@@ -0,0 +1,27 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.alterColumn("user_games", "hours_played", {
+    type: "integer",
+    using: "hours_played::integer",
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.alterColumn("user_games", "hours_played", {
+    type: "numeric",
+  });
+};
