3fbcb5016d099faa583c32974815197f1db11435
Bruna Polizeli <66567923+cloudcastaway@users.noreply.github.com>
2026-09-24 20:15:41 -0300

    chore: add total_playitme to user_games table

---
+++ server/migrations/1790290439073_add-total-playtime-to-user-games.js
@@ -0,0 +1,26 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.sql(`
+    ALTER TABLE user_games
+    ADD COLUMN total_playtime INTEGER
+    GENERATED ALWAYS AS (COALESCE(hours_played, 0) * 60 + COALESCE(minutes_played, 0)) STORED;
+  `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.sql(`ALTER TABLE user_games DROP COLUMN total_playtime;`);
+};
